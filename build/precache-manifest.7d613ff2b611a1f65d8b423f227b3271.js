fea75e52fed12af2d63ad8ca76d246611e435e68
edmsantos <edmsantos@uolinc.com>
2018-11-19 09:26:10 -0200

    gh pages

---
+++ build/precache-manifest.7d613ff2b611a1f65d8b423f227b3271.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f5084cc58f8e0cc92edb",
+    "url": "/static/js/main.f5084cc5.chunk.js"
+  },
+  {
+    "revision": "ac33a4f3b8d02b7bf883",
+    "url": "/static/js/1.ac33a4f3.chunk.js"
+  },
+  {
+    "revision": "f5084cc58f8e0cc92edb",
+    "url": "/static/css/main.3e40c43d.chunk.css"
+  },
+  {
+    "revision": "b68ba45f7a66074e0657643715a07056",
+    "url": "/index.html"
+  }
+];
